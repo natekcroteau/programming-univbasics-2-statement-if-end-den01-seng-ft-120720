910f902fb63f4d7e3834ef40612f1572a437aba0
Nate Croteau <natekcroteau@gmail.com>
2020-11-11 17:32:11 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,1 @@
 # Write your solution here
-
-chance_of_rain = -23
-if chance_of_rain <= 0.25
-    puts "Pack your sunscreen!"
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Better pack an umbrella!"
-else
-  puts "stay home and read a novel by the fire."
-end
