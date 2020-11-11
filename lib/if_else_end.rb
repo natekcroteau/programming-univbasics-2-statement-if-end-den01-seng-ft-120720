e572b35b3f9bc5b06aae9dec5bd5edc99639161b
Nate Croteau <natekcroteau@gmail.com>
2020-11-11 17:31:58 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
 # Write your solution here
 
-chance_of_rain = 1000
+chance_of_rain = -23
 if chance_of_rain <= 0.25
     puts "Pack your sunscreen!"
 elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
